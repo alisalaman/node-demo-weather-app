148966ad73cbf092431dab3db5f3bf365c5aa50a
alisalaman <asalaman@gmail.com>
2021-01-09 08:10:25 +0000

    Added wind speed to weather description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,6 @@ const request = require("postman-request")
 
 const forecast = (geo, callback) => {
     const url = "http://api.weatherstack.com/current?access_key=3c657d000005ec8c4a0efb1f8536a8c2&query=" + geo.latitude + "," + geo.longitude + "&units=m"
-    console.log("calling weatherstack URL: " + url)
 
     request({ url, json: true }, (error, { body }) => {
 
@@ -14,8 +13,11 @@ const forecast = (geo, callback) => {
 
         } else {
             const data = body.current
+
+            console.log(data)
+
             callback(undefined, {
-                description: "In " + geo.location + " it's currently " + data.temperature + " degrees and " + data.weather_descriptions[0] + ".  It feels like " + data.feelslike + " degrees",
+                description: "In " + geo.location + " it's currently " + data.temperature + " degrees and " + data.weather_descriptions[0] + ".  It feels like " + data.feelslike + " degrees and there is a wind speed of " + data.wind_speed,
                 temperature: data.temperature,
                 feelslike: data.feelslike
             })
